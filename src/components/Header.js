7cb162f27923933693d25a100f006e06de4e82b6
waleedd32 <valid0@hotmail.com>
2021-02-06 17:40:01 +0200

    added youtube logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,11 @@ function Header() {
     <div>
       <h1>This is header</h1>
       <MenuIcon />
+      <img
+        className="header__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+        alt=""
+      />
     </div>
   );
 }
