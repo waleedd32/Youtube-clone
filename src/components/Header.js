e0aedab94f72bb5e8eb9e75b271d188f75c122b9
waleedd32 <valid0@hotmail.com>
2021-02-07 23:57:08 +0200

    added AppsIcon & NotificationsIcon & Avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from "react";
 import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/avatar";
+
 function Header() {
   return (
     <div>
@@ -15,6 +19,12 @@ function Header() {
       <input type="text" />
       <SearchIcon className="header__inputButton" />
       <VideoCallIcon className="header__icon" />
+      <AppsIcon className="header__icon" />
+      <NotificationsIcon className="header__icon" />
+      <Avatar
+        alt="Remy Sharp"
+        src="https://avatars.githubusercontent.com/u/32420369?s=400&u=c53a0c0b6af70645e081570176c3a4732eadaad1&v=4"
+      />
     </div>
   );
 }
