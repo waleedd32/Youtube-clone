661f1f954d987ea6a68723c721361a8cf4dc19ec
waleedd32 <valid0@hotmail.com>
2021-02-13 12:55:35 +0200

    made three children

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,21 +10,27 @@ import Avatar from "@material-ui/core/avatar";
 function Header() {
   return (
     <div className="header">
-      <MenuIcon />
-      <img
-        className="header__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
-        alt=""
-      />
-      <input type="text" />
-      <SearchIcon className="header__inputButton" />
-      <VideoCallIcon className="header__icon" />
-      <AppsIcon className="header__icon" />
-      <NotificationsIcon className="header__icon" />
-      <Avatar
-        alt="Remy Sharp"
-        src="https://avatars.githubusercontent.com/u/32420369?s=400&u=c53a0c0b6af70645e081570176c3a4732eadaad1&v=4"
-      />
+      <div className="header__left">
+        <MenuIcon />
+        <img
+          className="header__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
+          alt=""
+        />
+      </div>
+      <div className="header__input">
+        <input type="text" />
+        <SearchIcon className="header__inputButton" />
+      </div>
+      <div className="header__icons">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+        <Avatar
+          alt="Remy Sharp"
+          src="https://avatars.githubusercontent.com/u/32420369?s=400&u=c53a0c0b6af70645e081570176c3a4732eadaad1&v=4"
+        />
+      </div>
     </div>
   );
 }
