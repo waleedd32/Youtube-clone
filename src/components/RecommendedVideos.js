727a8c40017effad6c359fc46dd667738f1ef1a5
waleedd32 <valid0@hotmail.com>
2021-03-02 17:57:55 +0200

    created Sidebar.css & RecommendedVideos.css

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./RecommendedVideos.css";
 function RecommendedVideos() {
   return (
-    <div>
+    <div className="recommendedVideos">
       <h2>This is where videos go</h2>
     </div>
   );
