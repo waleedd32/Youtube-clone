25bf30ee1b2d07247b755764b46736e6b6a773d8
waleedd32 <valid0@hotmail.com>
2021-03-05 23:56:47 +0200

    puting everything next to eachother

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Sidebar.css";
 function Sidebar() {
   return (
-    <div>
-      <h1>I'm a side bar</h1>
+    <div className="sidebar">
+      <h1>I'm a sidebar</h1>
     </div>
   );
 }
