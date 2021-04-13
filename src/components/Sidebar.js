61d631467219e52413c364a0dfe4b2ef01162e5f
waleedd32 <valid0@hotmail.com>
2021-04-13 20:16:38 +0300

    created sidebarRow.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import SidebarRow from "./SidebarRow";
 import "./Sidebar.css";
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>I'm a sidebar</h1>
+      <SidebarRow title="Home" />
+      <SidebarRow title="Trending" />
+      <SidebarRow title="Subscription" />
     </div>
   );
 }
