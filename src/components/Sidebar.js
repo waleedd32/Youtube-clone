4c2c51ba7b0f0829b71f490d57c247d6816ed828
waleedd32 <valid0@hotmail.com>
2021-04-17 15:17:59 +0300

    added material-ui HomeIcon & created SidebarRow.css

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SidebarRow from "./SidebarRow";
 import "./Sidebar.css";
+
 function Sidebar() {
   return (
     <div className="sidebar">
--- src/components/SidebarRow.js
+++ src/components/SidebarRow.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "./SidebarRow.css";
+import HomeIcon from "@material-ui/icons/Home";
 
 function SidebarRow({ title }) {
   return (
     <div className="sidebarRow">
+      <HomeIcon />
       <h2>{title}</h2>
     </div>
   );
