169f5ae6b328aa0037aba36d63186dd1836dd57e
waleedd32 <valid0@hotmail.com>
2021-05-28 20:09:22 +0300

    making sidebarRow(Home) by default active(selected)

--- src/components/SidebarRow.js
+++ src/components/SidebarRow.js
@@ -2,9 +2,10 @@ import React from "react";
 import "./SidebarRow.css";
 import HomeIcon from "@material-ui/icons/Home";
 
-function SidebarRow({ Icon, title }) {
+function SidebarRow({ selected, Icon, title }) {
   return (
-    <div className="sidebarRow">
+    <div className={`sidebarRow ${selected && "selected"} `}>
+      {" "}
       <Icon className="sidebarRow__icon" />
       <h2 className="sidebarRow__title"> {title}</h2>
     </div>
