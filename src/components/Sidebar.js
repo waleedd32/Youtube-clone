5649e08201ca058e233c1b89969133d80e73a94f
waleedd32 <valid0@hotmail.com>
2021-06-15 16:20:50 +0300

    added 6 more icons in Sidebar.js after hr

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,12 @@ import "./Sidebar.css";
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbDownAlt";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
   return (
@@ -12,6 +18,13 @@ function Sidebar() {
       <SidebarRow Icon={WhatshotIcon} title="Trending" />
       <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
 
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+      <SidebarRow Icon={HistoryIcon} title="History" />
+      <SidebarRow Icon={OndemandVideoIcon} title="Your videos" />
+      <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="Liked videos" />
+      <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Show more" />
       <hr />
     </div>
   );
