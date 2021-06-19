a924ac4c49fd82b313c0ab9b01e7c0ef46a3ff74
waleedd32 <valid0@hotmail.com>
2021-06-19 15:41:48 +0300

    added h2 to the RecommendedVideos.js and also some css styles like background color, padding to its parent class 'recommendedVideos'

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -3,7 +3,7 @@ import "./RecommendedVideos.css";
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
-      <h2>This is where videos go</h2>
+      <h2>Recommended</h2>
     </div>
   );
 }
