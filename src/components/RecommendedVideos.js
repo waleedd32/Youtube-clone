29b36d9ac44d07dbb68a94a2fb85a076af419fd7
waleedd32 <valid0@hotmail.com>
2021-06-28 11:20:02 +0300

    created VideoCard.js

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./RecommendedVideos.css";
+import VideoCard from "./VideoCard";
 function RecommendedVideos() {
   return (
     <div className="recommendedVideos">
       <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+      </div>
     </div>
   );
 }
---
+++ src/components/VideoCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
+  return (
+    <div>
+      <h4>video</h4>
+    </div>
+  );
+}
+
+export default VideoCard;
