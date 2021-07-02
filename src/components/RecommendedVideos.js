04dec34aca5aead359884e7f6bb6d6773b8dd03e
waleedd32 <valid0@hotmail.com>
2021-07-03 01:58:36 +0300

    added information as a props to one VideoCard component

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -6,7 +6,14 @@ function RecommendedVideos() {
     <div className="recommendedVideos">
       <h2>Recommended</h2>
       <div className="recommendedVideos__videos">
-        <VideoCard />
+        <VideoCard
+          title="Virtual Reality is real now !"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://www.nbc.com/sites/nbcunbc/files/images/2018/7/31/Heroes-KeyArt-Logo-Show-Tile-1920x1080.jpg"
+          channel="Sunny Sangha"
+          image="https://cnet2.cbsistatic.com/img/bsWelKlGqQiCmd9hoiiJaY7_-Z4=/940x0/2019/04/18/dc1a2cd4-58ad-4199-8145-25ab52acdfba/23-oculus-quest.jpg"
+        />
         <VideoCard />
         <VideoCard />
         <VideoCard />
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -11,6 +11,13 @@ function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
           alt={channel}
           src={channelImage}
         />
+        <div className="videoCard__text">
+          <h4>{title}</h4>
+          <p>{channel} </p>
+          <p>
+            {views}-{timestamp}
+          </p>
+        </div>
       </div>
     </div>
   );
