59c123e5258d0ad0b0e0e54c0b5a3803698aa592
waleedd32 <valid0@hotmail.com>
2021-07-06 00:23:43 +0300

    added information to seventh VideoCard component

--- src/components/RecommendedVideos.js
+++ src/components/RecommendedVideos.js
@@ -54,6 +54,14 @@ function RecommendedVideos() {
           channel="Sunny Sangha"
           image="https://www.3dorganon.com/wp-content/uploads/2020/10/oculus-quest-2-256.png"
         />
+        <VideoCard
+          title="Virtual Reality is real now !"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://www.nbc.com/sites/nbcunbc/files/images/2018/7/31/Heroes-KeyArt-Logo-Show-Tile-1920x1080.jpg"
+          channel="Sunny Sangha"
+          image="https://cnet2.cbsistatic.com/img/bsWelKlGqQiCmd9hoiiJaY7_-Z4=/940x0/2019/04/18/dc1a2cd4-58ad-4199-8145-25ab52acdfba/23-oculus-quest.jpg"
+        />
       </div>
     </div>
   );
