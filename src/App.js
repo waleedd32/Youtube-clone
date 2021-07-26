a4bdca806642c02ba7d956548864837ef27d40ea
waleedd32 <valid0@hotmail.com>
2021-07-26 15:35:16 +0300

    in App.js added react router (Router, Switch, Route)

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import "./App.css";
 import Header from "./components/Header";
 import RecommendedVideos from "./components/RecommendedVideos";
 import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    // BEM class naming convention
     <div className="app">
-      <h1>This is going to become a Youtube Clone !</h1>
-      {/* Header  -> <Header/> */}
-      <Header />
-      <div className="app__page">
-        <Sidebar />
-        <RecommendedVideos />
-      </div>
-
-      {/* Sidebar -> <Sidebar/>*/}
-      {/* RecommendedVideo */}
+      <Router>
+        {/* Header outside switch so it comes up always */}
+        <Header />
+        <Switch>
+          <Route path="/search/:searchTerm">
+            <div className="app__page">
+              <Sidebar />
+            </div>
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
