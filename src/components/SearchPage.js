992ba6514a8f1588f1ed1d78ad7c4a684186fb44
waleedd32 <valid0@hotmail.com>
2021-08-19 22:07:30 +0300

    added new icon when user has searched something appears at the top

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import TuneOutlinedIcon from "@material-ui/icons/TuneOutlined";
+// import TuneOutlined from "@material-ui/icons/TuneOutlined";
 
 function SearchPage() {
   return (
-    <div>
-      <h1>SearchPage</h1>
+    <div className="searchPages">
+      <div className="searchPages__filter">
+        <TuneOutlinedIcon />
+        <h2>filter</h2>
+      </div>
     </div>
   );
 }
