c3ebd6c48a57cfe80ce37ff7ba582047accac6fc
waleedd32 <valid0@hotmail.com>
2022-01-08 00:56:45 +0200

    added Avatar icon in ChannelRow

--- src/components/ChannelRow.js
+++ src/components/ChannelRow.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Avatar } from "@material-ui/core";
 
-function ChannelRow() {
-  return <div></div>;
+function ChannelRow({
+  image,
+  channel,
+  subs,
+  noOfVideos,
+  description,
+  verified,
+}) {
+  return (
+    <div className="channelRow">
+      <Avatar className="channelRow__logo" alt={channel} src={image} />
+    </div>
+  );
 }
 
 export default ChannelRow;
