c937d65db218df6c5364338dd2d45dd7ee78c6e2
waleedd32 <valid0@hotmail.com>
2022-01-21 14:07:43 +0200

    added VerifiedIcon in ChannelRow

--- src/components/ChannelRow.js
+++ src/components/ChannelRow.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Avatar } from "@material-ui/core";
+import VerifiedIcon from "@material-ui/icons/CheckCircleOutlineOutlined";
 
 function ChannelRow({
   image,
@@ -12,6 +13,11 @@ function ChannelRow({
   return (
     <div className="channelRow">
       <Avatar className="channelRow__logo" alt={channel} src={image} />
+      <div className="channelRow__text">
+        <h4>
+          {channel} {verified && <VerifiedIcon />}
+        </h4>
+      </div>
     </div>
   );
 }
