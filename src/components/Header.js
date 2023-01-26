f050c03c5e6eab6d403e16cad099b0b45878a559
waleedd32 <valid0@hotmail.com>
2023-01-26 16:44:47 +0200

    clear input when Youtube logo is clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,7 +27,7 @@ function Header() {
     <div className="header">
       <div className="header__left">
         <MenuIcon />
-        <Link to="/">
+        <Link to="/" onClick={() => setInputSearch("")}>
           <img
             className="header__logo"
             src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg"
